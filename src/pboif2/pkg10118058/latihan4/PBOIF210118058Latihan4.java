1b2da2d4408a682f22714d2f972e3e9485843cb5
SatyaningAndaru <55575075+SatyaningAndaru@users.noreply.github.com>
2019-09-24 17:54:06 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118058/latihan4/PBOIF210118058Latihan4.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118058.latihan4;
+
+/**
+ *
+ * @author user
+ * Nama         :   Satyaning Andaru Bawalaksana
+ * Nim          :   10118058
+ * Kelas        :   PBOIF2
+ * Deskripsi    :   Program ini berisi program menampilkan
+ * Jenis-jenis tipe data bilangan bulat
+ */
+public class PBOIF210118058Latihan4 {
+
+  
+     public void tambahKambing(){
+ 
+     //deklarasi variabel
+     int jumlahKambing = 0;
+     
+     jumlahKambing = jumlahKambing + 5;
+     
+     
+     System.out.println("Jumlah kambing setelah ditambah : " + jumlahKambing );
+     }
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBOIF210118058Latihan4 kambingJantan = new PBOIF210118058Latihan4();
+        kambingJantan.tambahKambing();
+    }
+  }
+
